fb92f3a2f786d39de0927c14ba20d82bd3f30425
Brent Ransom <brent.e.ransom@gmail.com>
2014-03-23 23:05:22 -0400

    Homepage

--- wp-content/themes/Metzger_HVAC_Child/footer.php
+++ wp-content/themes/Metzger_HVAC_Child/footer.php
@@ -2,15 +2,17 @@
 	<div class="row">
 		<!-- Pinterest or Instagram Feed Footer -->
 		<div class="large-4 medium-4 small-12 columns">
-			
+			<h2>Pinterest Before & After Feed</h2>
+			<?php echo do_shortcode('[prw username="bkmacdaddy" boardname="design-inspiration" maxfeeds="10" divname="myList" printtext="0" target="newwindow" useenclosures="yes" thumbwidth="50" thumbheight="50" showfollow=""]
+'); ?>
 		</div>
 		<!-- Contact Info Footer  -->
 		<div class="large-4 medium-4 small-12 columns">
-			<?php echo get_field('contact_info'); ?>
+			<?php echo get_field('contact_info', 'option'); ?>
 		</div>
 		<!-- Our Promise Footer -->
 		<div class="large-4 medium-4 small-12 columns">
-			<?php echo get_field('promise_text'); ?>
+			<?php echo get_field('promise_text', 'option'); ?>
 		</div>
 	</div>	
 </footer>
